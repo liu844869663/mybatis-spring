308a79a037b66661e2f636ae5380bee2572f20e3
刘景平 <liujingping@idwzx.com>
2019-12-11 10:03:16 +0800

    1.在SqlSession执行sql时通过用SpringManagedTransaction代替mybatis的JdbcTransaction，让SqlSession从spring的ThreadLocal中获取jdbc connection。
    2.通过注册事务生命周期callback接口SqlSessionSynchronization，让SqlSession有机会在spring管理的事务提交或回滚时清理自己的内部缓存。

--- src/main/java/org/mybatis/spring/batch/MyBatisBatchItemWriter.java
+++ src/main/java/org/mybatis/spring/batch/MyBatisBatchItemWriter.java
@@ -37,13 +37,15 @@ import org.springframework.dao.InvalidDataAccessResourceUsageException;
  * {@code SqlSessionTemplate} to execute a batch of statements for all items
  * provided.<br/>
  *
- * Provided to facilitate the migration from Spring-Batch iBATIS 2 writers to MyBatis 3<br/>
+ * Provided to facilitate the migration from Spring-Batch iBATIS 2 writers to
+ * MyBatis 3<br/>
  *
- * The user must provide a MyBatis statement id that points to the SQL statement defined
- * in the MyBatis.<br/>
+ * The user must provide a MyBatis statement id that points to the SQL statement
+ * defined in the MyBatis.<br/>
  *
- * It is expected that {@link #write(List)} is called inside a transaction. If it is not
- * each statement call will be autocommitted and flushStatements will return no results.<br/>
+ * It is expected that {@link #write(List)} is called inside a transaction. If
+ * it is not each statement call will be autocommitted and flushStatements will
+ * return no results.<br/>
  *
  * The writer is thread safe after its properties are set (normal singleton
  * behavior), so it can be used to write in multiple concurrent transactions.
@@ -54,99 +56,101 @@ import org.springframework.dao.InvalidDataAccessResourceUsageException;
  */
 public class MyBatisBatchItemWriter<T> implements ItemWriter<T>, InitializingBean {
 
-  private static final Log LOGGER = LogFactory.getLog(MyBatisBatchItemWriter.class);
+	private static final Log LOGGER = LogFactory.getLog(MyBatisBatchItemWriter.class);
 
-  private SqlSessionTemplate sqlSessionTemplate;
+	private SqlSessionTemplate sqlSessionTemplate;
 
-  private String statementId;
+	private String statementId;
 
-  private boolean assertUpdates = true;
+	private boolean assertUpdates = true;
 
-  /**
-   * Public setter for the flag that determines whether an assertion is made
-   * that all items cause at least one row to be updated.
-   *
-   * @param assertUpdates the flag to set. Defaults to true;
-   */
-  public void setAssertUpdates(boolean assertUpdates) {
-    this.assertUpdates = assertUpdates;
-  }
+	/**
+	 * Public setter for the flag that determines whether an assertion is made that
+	 * all items cause at least one row to be updated.
+	 *
+	 * @param assertUpdates the flag to set. Defaults to true;
+	 */
+	public void setAssertUpdates(boolean assertUpdates) {
+		this.assertUpdates = assertUpdates;
+	}
 
-  /**
-   * Public setter for {@link SqlSessionFactory} for injection purposes.
-   *
-   * @param sqlSessionFactory a factory object for the {@link SqlSession}.
-   */
-  public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
-    if (sqlSessionTemplate == null) {
-      this.sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory, ExecutorType.BATCH);
-    }
-  }
+	/**
+	 * Public setter for {@link SqlSessionFactory} for injection purposes.
+	 *
+	 * @param sqlSessionFactory a factory object for the {@link SqlSession}.
+	 */
+	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+		if (sqlSessionTemplate == null) {
+			this.sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory, ExecutorType.BATCH);
+		}
+	}
 
-  /**
-   * Public setter for the {@link SqlSessionTemplate}.
-   *
-   * @param sqlSessionTemplate a template object for use the {@link SqlSession} on the Spring managed transaction
-   */
-  public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
-    this.sqlSessionTemplate = sqlSessionTemplate;
-  }
+	/**
+	 * Public setter for the {@link SqlSessionTemplate}.
+	 *
+	 * @param sqlSessionTemplate a template object for use the {@link SqlSession} on
+	 *                           the Spring managed transaction
+	 */
+	public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
+		this.sqlSessionTemplate = sqlSessionTemplate;
+	}
 
-  /**
-   * Public setter for the statement id identifying the statement in the SqlMap
-   * configuration file.
-   *
-   * @param statementId the id for the statement
-   */
-  public void setStatementId(String statementId) {
-    this.statementId = statementId;
-  }
+	/**
+	 * Public setter for the statement id identifying the statement in the SqlMap
+	 * configuration file.
+	 *
+	 * @param statementId the id for the statement
+	 */
+	public void setStatementId(String statementId) {
+		this.statementId = statementId;
+	}
 
-  /**
-   * Check mandatory properties - there must be an SqlSession and a statementId.
-   */
-  @Override
-  public void afterPropertiesSet() {
-    notNull(sqlSessionTemplate, "A SqlSessionFactory or a SqlSessionTemplate is required.");
-    isTrue(ExecutorType.BATCH == sqlSessionTemplate.getExecutorType(), "SqlSessionTemplate's executor type must be BATCH");
-    notNull(statementId, "A statementId is required.");
-  }
+	/**
+	 * Check mandatory properties - there must be an SqlSession and a statementId.
+	 */
+	@Override
+	public void afterPropertiesSet() {
+		notNull(sqlSessionTemplate, "A SqlSessionFactory or a SqlSessionTemplate is required.");
+		isTrue(ExecutorType.BATCH == sqlSessionTemplate.getExecutorType(),
+				"SqlSessionTemplate's executor type must be BATCH");
+		notNull(statementId, "A statementId is required.");
+	}
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void write(final List<? extends T> items) {
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void write(final List<? extends T> items) {
 
-    if (!items.isEmpty()) {
+		if (!items.isEmpty()) {
 
-      if (LOGGER.isDebugEnabled()) {
-        LOGGER.debug("Executing batch with " + items.size() + " items.");
-      }
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Executing batch with " + items.size() + " items.");
+			}
 
-      for (T item : items) {
-        sqlSessionTemplate.update(statementId, item);
-      }
+			for (T item : items) {
+				sqlSessionTemplate.update(statementId, item);
+			}
 
-      List<BatchResult> results = sqlSessionTemplate.flushStatements();
+			List<BatchResult> results = sqlSessionTemplate.flushStatements();
 
-      if (assertUpdates) {
-        if (results.size() != 1) {
-          throw new InvalidDataAccessResourceUsageException("Batch execution returned invalid results. " +
-              "Expected 1 but number of BatchResult objects returned was " + results.size());
-        }
+			if (assertUpdates) {
+				if (results.size() != 1) {
+					throw new InvalidDataAccessResourceUsageException("Batch execution returned invalid results. "
+							+ "Expected 1 but number of BatchResult objects returned was " + results.size());
+				}
 
-        int[] updateCounts = results.get(0).getUpdateCounts();
+				int[] updateCounts = results.get(0).getUpdateCounts();
 
-        for (int i = 0; i < updateCounts.length; i++) {
-          int value = updateCounts[i];
-          if (value == 0) {
-            throw new EmptyResultDataAccessException("Item " + i + " of " + updateCounts.length
-                + " did not update any rows: [" + items.get(i) + "]", 1);
-          }
-        }
-      }
-    }
-  }
+				for (int i = 0; i < updateCounts.length; i++) {
+					int value = updateCounts[i];
+					if (value == 0) {
+						throw new EmptyResultDataAccessException("Item " + i + " of " + updateCounts.length
+								+ " did not update any rows: [" + items.get(i) + "]", 1);
+					}
+				}
+			}
+		}
+	}
 
 }
